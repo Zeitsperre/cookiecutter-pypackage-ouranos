ad47b2ab934502662a3b1acd8176243aa7591db5
Zeitsperre <trevor_smith@live.com>
2019-08-15 14:26:32 -0400

    More mimicking of the xclim package boilerplate configuration

--- {{cookiecutter.repo_name}}/test/test_{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/test/test_{{cookiecutter.repo_name}}.py
@@ -2,8 +2,14 @@
 Tests for `{{ cookiecutter.repo_name }}` module.
 """
 import pytest
+import {{ cookiecutter.repo_name }}
+
 from {{ cookiecutter.repo_name }} import {{ cookiecutter.repo_name }}
 
+class Test{{ cookiecutter.repo_name|capitalize }}Version(object):
+
+    def test_version(self):
+        assert {{ cookiecutter.repo_name }}.__version__
 
 class Test{{ cookiecutter.repo_name|capitalize }}(object):
 
